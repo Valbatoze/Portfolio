9c13ebaf8dc8dc2e24c25acacf7046f5abbad373
Valbatoze <edwin.febvin@ynov.com>
2022-11-27 18:02:57 +0100

    ^ajout^ du^ publicpath^

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const publicPath = process.env.NODE_ENV === 'production' ? '/portfolio/' : '/'
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+ export default defineConfig({
+  publicPath: publicPath,
+   plugins: [react()]
+ })
